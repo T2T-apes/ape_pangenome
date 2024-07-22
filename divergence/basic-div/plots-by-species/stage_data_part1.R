45ebaabe9b5604a820784fcbe7c8e0dbe9249f6b
Erin  Molloy <ekmolloy@vpn-118.umiacs.umd.edu>
2024-07-22 12:39:57 -0400

    Updated

--- divergence/basic-div/plots-by-species/stage_data_part1.R
+++ divergence/basic-div/plots-by-species/stage_data_part1.R
@@ -26,7 +26,7 @@ intra_df5$NAME = "PonAbe1"
 
 intra_df6 <- read.csv("../basic-div-mPonPyg2#1-mPonPyg2#1-vs-mPonPyg2#2-autosomes.csv",
                 header=TRUE, row.names=NULL, sep=',')
-intra_df6$NAME = "mPonPyg2"
+intra_df6$NAME = "PonPyg2"
 
 # Human vs. Chimp
 df1a <- read.csv("../basic-div-hg002#M-hg002#M-vs-mPanTro3#1-autosomes.csv",
@@ -116,7 +116,7 @@ df9x <- read.csv("../basic-div-hg002#M-hg002#M-vs-mPonPyg2#1-X.csv",
 df9y <- read.csv("../basic-div-hg002#P-hg002#P-vs-mPonPyg2#2-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df9 = rbind(df9a, df9x, df9y)
-df9$NAME = "hg002 vs. mPonPyg2"
+df9$NAME = "hg002 vs. PonPyg2"
 
 # B. Orang vs. Human
 df10a <- read.csv("../basic-div-mPonPyg2#1-mPonPyg2#1-vs-hg002#M-autosomes.csv",
@@ -126,7 +126,7 @@ df10x <- read.csv("../basic-div-mPonPyg2#1-mPonPyg2#1-vs-hg002#M-X.csv",
 df10y <- read.csv("../basic-div-mPonPyg2#2-mPonPyg2#2-vs-hg002#P-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df10 = rbind(df10a, df10x, df10y)
-df10$NAME = "mPonPyg2 vs. hg002"
+df10$NAME = "PonPyg2 vs. hg002"
 
 # Adding S. Orang vs. B. Orang
 df11a <- read.csv("../basic-div-mPonAbe1#1-mPonAbe1#1-vs-mPonPyg2#1-autosomes.csv",
@@ -136,7 +136,7 @@ df11x <- read.csv("../basic-div-mPonAbe1#1-mPonAbe1#1-vs-mPonPyg2#1-X.csv",
 df11y <- read.csv("../basic-div-mPonAbe1#2-mPonAbe1#2-vs-mPonPyg2#2-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df11 = rbind(df11a, df11x, df11y)
-df11$NAME = "mPonAbe1 vs. mPonPyg2"
+df11$NAME = "PonAbe1 vs. PonPyg2"
 
 # Adding B. Orang vs. S. Orang
 df12a <- read.csv("../basic-div-mPonPyg2#1-mPonPyg2#1-vs-mPonAbe1#1-autosomes.csv",
@@ -146,7 +146,7 @@ df12x <- read.csv("../basic-div-mPonPyg2#1-mPonPyg2#1-vs-mPonAbe1#1-X.csv",
 df12y <- read.csv("../basic-div-mPonPyg2#2-mPonPyg2#2-vs-mPonAbe1#2-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df12 = rbind(df12a, df12x, df12y)
-df12$NAME = "mPonPyg2 vs. mPonAbe1"
+df12$NAME = "PonPyg2 vs. PonAbe1"
 
 # Adding Chimp vs. Bonobo
 df13a <- read.csv("../basic-div-mPanTro3#1-mPanTro3#1-vs-mPanPan1#M-autosomes.csv",
@@ -156,7 +156,7 @@ df13x <- read.csv("../basic-div-mPanTro3#1-mPanTro3#1-vs-mPanPan1#M-X.csv",
 df13y <- read.csv("../basic-div-mPanTro3#2-mPanTro3#2-vs-mPanPan1#P-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df13 = rbind(df13a, df13x, df13y)
-df13$NAME = "mPanTro3 vs. PanPan1"
+df13$NAME = "PanTro3 vs. PanPan1"
 
 # Adding Bonobo vs. Chimp
 df14a <- read.csv("../basic-div-mPanPan1#M-mPanPan1#M-vs-mPanTro3#1-autosomes.csv",
@@ -166,7 +166,7 @@ df14x <- read.csv("../basic-div-mPanPan1#M-mPanPan1#M-vs-mPanTro3#1-X.csv",
 df14y <- read.csv("../basic-div-mPanPan1#P-mPanPan1#P-vs-mPanTro3#2-Y.csv",
                  header=TRUE, row.names=NULL, sep=',')
 df14 = rbind(df14a, df14x, df14y)
-df14$NAME = "PanPan1 vs. mPanTro3"
+df14$NAME = "PanPan1 vs. PanTro3"
 
 # Combine data
 df = rbind(intra_df1, intra_df2, intra_df3, intra_df4, intra_df5, intra_df6, 
@@ -183,9 +183,9 @@ names <- c("hg002", "PanTro3", "PanPan1", "GorGor1", "PonAbe1", "mPonPyg2",
            "hg002 vs. PanPan1", "PanPan1 vs. hg002",
            "hg002 vs. GorGor1", "GorGor1 vs. hg002",
            "hg002 vs. PonAbe1", "PonAbe1 vs. hg002",
-           "hg002 vs. mPonPyg2", "mPonPyg2 vs. hg002",
-           "mPonAbe1 vs. mPonPyg2", "mPonPyg2 vs. mPonAbe1",
-           "mPanTro3 vs. PanPan1", "PanPan1 vs. mPanTro3")
+           "hg002 vs. PonPyg2", "PonPyg2 vs. hg002",
+           "PonAbe1 vs. PonPyg2", "PonPyg2 vs. PonAbe1",
+           "PanTro3 vs. PanPan1", "PanPan1 vs. PanTro3")
 
 df$NAME <- factor(df$NAME, levels=names)
 
